4fb63e196688e7014d46bc232f1e3413e0e32d52
qui.le <nick.le@g.com>
2023-05-06 14:45:41 +0700

    update src

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,55 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+// const key = './../dev.local+1-key.pem';
+
+// const certPath = './../dev.local+1-key.pem'
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  // app.enableCors();
+  // await app.listen(3002);
+
+
+  // // http://localhost:3002/
+
+  // const port = Number(process.env.PORT) || 3002;
+  // const hostname = process.env.HOSTNAME || 'localhost';
+  // await app.listen(port, hostname, () => {
+  //   const address =
+  //     'https' + '://' + hostname + ':' + port + '/';
+  //   Logger.log('Listening at ' + address);
+  // });
+
+
+
+  const ssl = true;//process.env.SSL === 'true' ? true : false;
+  let httpsOptions = null;
+  if (ssl) {
+    const keyPath = './../dev.local+1-key.pem' || '';
+    const certPath = './../dev.local+1.pem' || '';
+    httpsOptions = {
+      key: fs.readFileSync(path.join(__dirname, keyPath)),
+      cert: fs.readFileSync(path.join(__dirname, certPath)),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
-  await app.listen(3002);
+
+  const port = Number(process.env.PORT) || 3002;
+  const hostname = process.env.HOSTNAME || '18.140.70.28';
+  await app.listen(port, hostname, () => {
+    const address =
+      'http' + (ssl ? 's' : '') + '://' + hostname + ':' + port + '/';
+    Logger.log('Listening at ' + address);
+  });
+
+
 }
 bootstrap();
